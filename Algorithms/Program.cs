cdd13e11ff9c35b25b753455096a93b82b70c04f
patryk-wolski <patryk.wolski@flexdevgroup.com>
2022-04-13 17:12:32 +0100

    init

---
+++ Algorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
